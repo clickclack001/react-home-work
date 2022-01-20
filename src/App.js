022fa6fa7b8fe63b799307dcfb2b46b0be6df7f1
freelancer007 <freelancer007@yandex.ru>
2022-01-21 02:20:39 +0400

    Сделал ДЗ №1 (Б6У4)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { Paper, Divider, Button, List, Tabs, Tab } from '@mui/material'
+import { AddField } from './components/AddField'
+import { Item } from './components/Item'
+import { useReducer } from 'react'
 
-function App() {
-    return (
-        <div className="App">
-
-        </div>
-    );
+function reducer(state, action) {
+  if (action.type === 'ADD_TASK') {
+    let maxId = state.reduce((prev, cur) => (prev.id > cur.id ? prev : cur))
+    action.newTask.id = maxId.id + 1
+    return [...state, action.newTask]
+  }
+  if (action.type === 'SET_COMPLETED') {
+    let newState = state.map((task) =>
+      task.id === action.taskId
+        ? { ...task, completed: !task.completed }
+        : task,
+    )
+    return [...newState]
+  }
+  return state
 }
 
-export default App;
+function App() {
+  const [state, dispatch] = useReducer(reducer, [
+    {
+      id: null,
+      text: '',
+      completed: false,
+    },
+  ])
+
+  const addTask = (task) => {
+    dispatch({
+      type: 'ADD_TASK',
+      newTask: task,
+    })
+  }
+
+  const setCompleted = (id) => {
+    dispatch({
+      type: 'SET_COMPLETED',
+      taskId: id,
+    })
+  }
+  return (
+    <div className="App">
+      <Paper className="wrapper">
+        <Paper className="header" elevation={0}>
+          <h4>Список задач</h4>
+        </Paper>
+        <AddField onAddTask={addTask} />
+        <Divider />
+        <Tabs value={0}>
+          <Tab label="Все" />
+          <Tab label="Активные" />
+          <Tab label="Завершённые" />
+        </Tabs>
+        <Divider />
+        <List>
+          {state
+            .filter((task) => task.id !== null)
+            .map((task) => (
+              <Item
+                completed={task.completed}
+                text={task.text}
+                key={task.id}
+                id={task.id}
+                onSetCompleted={setCompleted}
+              />
+            ))}
+        </List>
+        <Divider />
+        <div className="check-buttons">
+          <Button>Отметить всё</Button>
+          <Button>Очистить</Button>
+        </div>
+      </Paper>
+    </div>
+  )
+}
+
+export default App
